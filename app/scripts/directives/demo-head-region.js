564dbeb949d542a32a10583a58de55e82d3cfe17
Ann <qingcheng0532@gmail. com>
2015-03-27 15:34:49 +0800

    hello

--- app/scripts/directives/demo-head-region.js
+++ app/scripts/directives/demo-head-region.js
@@ -1,3 +1,16 @@
 /**
  * Created by panma on 3/27/15.
  */
+angular.module('demoApp')
+  .directive('demoHeadRegion',[function(){
+    return {
+
+      restrict: 'E',
+      templateUrl:'views/directives/demo-head-region.html',
+      link:function(scope,element){
+
+      }
+
+    }
+
+  }]);
--- app/scripts/directives/demo-main-region.js
+++ app/scripts/directives/demo-main-region.js
@@ -1,3 +1,16 @@
 /**
  * Created by panma on 3/27/15.
  */
+angular.module('demoApp')
+  .directive('demoMainRegion',[function(){
+    return {
+
+      restrict: 'E',
+      templateUrl:'views/directives/demo-main-region.html',
+      link:function(scope,element){
+
+      }
+
+    }
+
+  }]);
